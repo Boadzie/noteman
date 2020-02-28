a2cc343c77e18edcb60c81a6126f68e8446633d2
Boadzie Daniel <boadziedaniel43@gmail.com>
2020-02-28 14:32:14 +0000

    created a GraphQL API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Expresss!');
-});
+let notes = [
+  {
+    id: '1',
+    content: 'This is a note',
+    author: 'Adam Scott'
+  },
+  {
+    id: '2',
+    content: 'This is another note',
+    author: 'Harlow Everly'
+  },
+  {
+    id: '3',
+    content: 'Oh hey look, another note!',
+    author: 'Riley Harrison'
+  }
+];
 
-app.listen(port, () => {
-  console.log(`Server started on ${port}`);
+const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
+  type Query {
+    hello: String
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+`;
+
+// our resolver
+const resolvers = {
+  Query: {
+    hello: () => 'I love GraphQL!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Boadzie Daniel'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
+  }
+};
+
+// set up server
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// add middleware
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () => {
+  console.log(`GraphQL Server running on ${port} ${server.graphqlPath}`);
 });
